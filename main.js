5ae307336a17824518a4d3fb2eaf964f78cb56ea
Harrison Truong <harrisontruong@Harrisons-MacBook-Air.local>
2023-01-25 12:30:28 -0500

    added save button

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const btnEl = document.getElementById('increment-btn')
+const saveBtn = document.getElementById('save-btn')
+const saveEl = document.getElementById('save-el')
+
 btnEl.addEventListener('click',increment)
+saveBtn.addEventListener('click',save)
+
 let count = 0
-document.getElementById("count-el").innerHTML = count
+document.getElementById("count-el").textContent = count
 
 
 
 function increment() {
-  console.log(this)
   count += 1
-  document.getElementById("count-el").innerHTML = count
+  document.getElementById("count-el").textContent = count
+}
+
+function save() {
+  const saved = count + " - "
+  saveEl.textContent += saved
+  count = 0
+  document.getElementById("count-el").textContent = count
 }
